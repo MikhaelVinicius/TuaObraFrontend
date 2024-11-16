38c153829d8deaaa2d2789a258df47026976e4df
Your Name <you@example.com>
2024-11-16 17:18:05 -0300

    Responsividade do navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,18 +1,37 @@
-// src/components/Navbar.js
-import React from 'react';
-import '../style/Navbar.css'; // Estilo para o Navbar
+
+
+import '../style/Navbar.css'; 
+
+import React, { useState } from 'react';
+
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(true);
+
+  const toggleNavbar = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <nav className="navbar">
-      <h2 className="navbar-title">Menu</h2>
-      <ul className="navbar-list">
-        <li><a href="#home">Home</a></li>
-        <li><a href="#about">Sobre</a></li>
-        <li><a href="#services">Serviços</a></li>
-        <li><a href="#contact">Contato</a></li>
-      </ul>
-    </nav>
+    <div>
+      
+      <button className="navbar-toggle" onClick={toggleNavbar}>
+        ☰
+      </button>
+
+    
+      {isOpen && (
+        <nav className="navbar">
+          <h2 className="navbar-title">Menu</h2>
+          <ul className="navbar-list">
+            <li><a href="#home">Home</a></li>
+            <li><a href="#about">Sobre</a></li>
+            <li><a href="#services">Serviços</a></li>
+            <li><a href="#contact">Contato</a></li>
+          </ul>
+        </nav>
+      )}
+    </div>
   );
 };
 
