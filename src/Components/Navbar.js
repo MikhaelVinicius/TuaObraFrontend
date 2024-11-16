8e3f9a8b94466c1232fa311dd0dc10cf30b79401
Your Name <you@example.com>
2024-11-16 18:27:23 -0300

    Corrijido bug do navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,23 +1,36 @@
 
 
-import '../style/Navbar.css';
-import React from 'react';
+import '../style/Navbar.css'; 
 
-const Navbar = ({ onLinkClick }) => {
+import React, { useState } from 'react';
+
+
+const Navbar =({ onLinkClick }) => {
+  const [isOpen, setIsOpen] = useState(true);
+
+  const toggleNavbar = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <div>
-      <button className="navbar-toggle">
+      
+      <button className="navbar-toggle" onClick={toggleNavbar}>
         ☰
       </button>
 
-      <nav className="navbar">
-        <h2 className="navbar-title">Menu</h2>
-        <ul className="navbar-list">
+    
+      {isOpen && (
+        <nav className="navbar">
+          <h2 className="navbar-title">Menu</h2>
+          <ul className="navbar-list">
           <li><a href="#client" onClick={() => onLinkClick('client')}>Clientes</a></li>
           <li><a href="#dados" onClick={() => onLinkClick('dados')}>Meus dados</a></li>
-          <li><a href="#sair">Sair</a></li>
-        </ul>
-      </nav>
+            <li><a href="#sair">Sair</a></li>
+            
+          </ul>
+        </nav>
+      )}
     </div>
   );
 };
